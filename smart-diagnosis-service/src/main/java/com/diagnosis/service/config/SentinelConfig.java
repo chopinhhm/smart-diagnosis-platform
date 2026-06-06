1bee967bf7f00efe1924e57208619a15a973c2f6
黄浩茗 <1716496338@qq.com>
2026-06-06 11:26:05 +0800

    init: Sentinel熔断降级配置

---
+++ smart-diagnosis-service/src/main/java/com/diagnosis/service/config/SentinelConfig.java
@@ -0,0 +1,18 @@
+package com.diagnosis.service.config;
+
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Sentinel 熔断降级配置
+ * 开启 @SentinelResource 注解支持
+ */
+@Configuration
+public class SentinelConfig {
+
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
+    }
+}
