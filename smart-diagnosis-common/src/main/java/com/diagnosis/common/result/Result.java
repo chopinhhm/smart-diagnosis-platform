109bb924f8750942624962a9f50df1364de06198
黄浩茗 <1716496338@qq.com>
2026-06-06 11:26:16 +0800

    init: 统一响应

---
+++ smart-diagnosis-common/src/main/java/com/diagnosis/common/result/Result.java
@@ -0,0 +1,32 @@
+package com.diagnosis.common.result;
+
+import lombok.Data;
+
+/**
+ * 统一响应包装类
+ */
+@Data
+public class Result<T> {
+    private int code;
+    private String message;
+    private T data;
+
+    public static <T> Result<T> success(T data) {
+        Result<T> r = new Result<>();
+        r.setCode(200);
+        r.setMessage("success");
+        r.setData(data);
+        return r;
+    }
+
+    public static <T> Result<T> success() {
+        return success(null);
+    }
+
+    public static <T> Result<T> error(int code, String message) {
+        Result<T> r = new Result<>();
+        r.setCode(code);
+        r.setMessage(message);
+        return r;
+    }
+}
